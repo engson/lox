0c35436c8164f9fbe4339ffcfc6a36721b06acaf
engson <corastweb94@hotmail.com>
2026-08-09 21:27:08 +0200

    Added java

--- jlox/devenv.nix
+++ jlox/devenv.nix
@@ -10,6 +10,7 @@
   # https://devenv.sh/languages/
   # languages.rust.enable = true;
   languages.java.enable = true;
+  languages.java.lsp.enable = true;
   # https://devenv.sh/processes/
   # processes.dev.exec = "${lib.getExe pkgs.watchexec} -n -- ls -la";
 
